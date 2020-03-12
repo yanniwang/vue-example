fe3454768e2464e0a4ce7d70fabe9fae9d2eb9c2
dreamstarter <2412991330@qq.com>
2020-03-12 17:40:05 +0800

    Add files via upload

---
+++ 父子组件传值/child.vue
@@ -0,0 +1,35 @@
+<template>
+  <!--  <ul>-->
+  <!--    <li>我是子组件</li>-->
+  <!--    <li v-for="(item, index) in list">{{item}}</li>-->
+  <!--  </ul>-->
+  <p>我是子组件</p>
+</template>
+
+<script>
+  export default {
+    name: "child",
+
+    //子组件从父组件那里通过props获得数据
+    // props: {
+    //   list: {
+    //     type: Array,
+    //     default() {
+    //       return [22]
+    //     }
+    //   }
+    // },
+    data() {
+      return {
+        // array: [33, 78, 67]
+      }
+    },
+    mounted() {
+      this.$emit('getMessage', '我是父组件');
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 父子组件传值/father.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{title}}</p>
+    <!--    <child :list='list'></child>-->
+    <child @getMessage="message"></child>
+  </div>
+
+</template>
+
+<script>
+  import Child from "./child";
+
+  export default {
+    name: "father",
+    components: {Child},
+    data() {
+      return {
+        // list: [12, 67]
+        title: '111'
+      }
+    },
+    methods: {
+      message(title) {
+        this.title = title;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
